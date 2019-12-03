0993b99b428d4d77edada8b633f3dd0bb5ef8380
Abhinav Sharma <abhi18av@gmail.com>
2019-12-03 20:53:42 +0530

    experiment on a separate file

--- src/MaterialUI.js
+++ src/MaterialUI.js
@@ -1,4 +1,102 @@
 import React from "react";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Paper,
+  Link,
+  Container
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
 
-const MaterialUI = () => <div></div>;
+const useStyles = makeStyles(theme => ({
+  root: {
+    padding: theme.spacing(3, 2),
+    display: "flex",
+    flexDirection: "column",
+    minHeight: "100vh"
+  },
+  main: {
+    marginTop: theme.spacing(8),
+    marginBottom: theme.spacing(2)
+  },
+  footer: {
+    padding: theme.spacing(3, 2),
+    marginTop: "auto",
+    backgroundColor:
+      theme.palette.type === "dark"
+        ? theme.palette.grey[800]
+        : theme.palette.grey[200]
+  }
+}));
+
+const Header = () => <AppBar position="static">AppBar</AppBar>;
+
+function Footer() {
+  const classes = useStyles({});
+
+  return (
+    <Paper className={classes.root}>
+      <Typography variant="h5" component="h3" color="inherit">
+        This is a sheet of paper.
+      </Typography>
+      <Typography component="p">
+        Paper can be used to build surface or other elements for your
+        application.
+      </Typography>
+    </Paper>
+  );
+}
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary">
+      {"Copyright © "}
+      <Link color="inherit" href="https://material-ui.com/">
+        Your Website
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
+
+export function StickyFooter() {
+  const classes = useStyles({});
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <Container component="main" className={classes.main} maxWidth="sm">
+        <Typography variant="h2" component="h1" gutterBottom>
+          Sticky footer
+        </Typography>
+        <Typography variant="h5" component="h2" gutterBottom>
+          {"Pin a footer to the bottom of the viewport."}
+          {"The footer will move as the main element of the page grows."}
+        </Typography>
+        <Typography variant="body1">Sticky footer placeholder.</Typography>
+      </Container>
+      <footer className={classes.footer}>
+        <Container maxWidth="sm">
+          <Typography variant="body1">
+            My sticky footer can be found here.
+          </Typography>
+          <Copyright />
+        </Container>
+      </footer>
+    </div>
+  );
+}
+
+const MaterialUI = () => {
+  return (
+    <>
+      <Header />
+      <Footer />
+      <StickyFooter />
+    </>
+  );
+};
 export default MaterialUI;
